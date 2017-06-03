a5ddf734bb8d6ea02f8a613af7ad36ada086cbd9
Alexg2195 <alexg2195@gmail.com>
2017-06-03 12:27:49 -0500

    Folders and example components

---
+++ src/Layouts/CodeEditor.js
@@ -0,0 +1,27 @@
+/*
+ *
+ * CodeEditor
+ *
+ */
+
+import React from 'react';
+import Radium from 'radium';
+
+class CodeEditor extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        Hello
+      </div>
+    );
+  }
+}
+
+export default Radium(CodeEditor);
