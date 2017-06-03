401c24953e37aa58c90a0a8b928a36ebba7097a4
Lyle <denmanlp@gmail.com>
2017-06-03 17:45:33 -0500

    Add material-ui Card for output

--- src/Components/OutputArea.js
+++ src/Components/OutputArea.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import {Card, CardText} from 'material-ui/Card';
 
 class OutputArea extends React.Component {
   render() {
     const { editorOutput } = this.props;
     return (
-      <pre className="output">{ editorOutput }</pre>
+      <Card className="output">
+        <CardText>
+          { editorOutput }
+        </CardText>
+      </Card>
     )
   }
 }
