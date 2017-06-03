42c2706037910ce374d74e87b4c2a549831b0d75
Latif Masud <latif.masud@hotmail.com>
2017-06-03 18:07:06 -0500

    Adding in changes for themes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
 import React, { Component } from 'react';
 import CodeEditor from './Layouts/CodeEditor';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+
+import {kiwiGreen, kiwiLightGreen, kiwiPurple, kiwiLightPurple, kiwiBlue,
+        kiwiDarkBlue, kiwiLightRed, kiwiWhite, kiwiYellow, kiwiLimeGreen,
+        kiwiPastel, kiwiLightBlue, kiwiDarkGreen} from './colors';
+
+const main_theme = getMuiTheme({
+  palette: {
+    primary1Color: kiwiPurple,
+    primary2Color: kiwiLightPurple,
+    accent1Color: kiwiGreen,
+    accent2Color: kiwiLightGreen
+  }
+});
+
+const alt_theme1 = getMuiTheme({
+  palette: {
+    primary1Color: kiwiDarkBlue,
+    primary2Color: kiwiLightRed,
+    accent1Color: kiwiWhite,
+    accent2Color: kiwiYellow
+  }
+});
+
+const alt_theme2 = getMuiTheme({
+  palette: {
+    primary1Color: kiwiLimeGreen,
+    primary2Color: kiwiPastel,
+    accent1Color: kiwiLightBlue,
+    accent2Color: kiwiDarkGreen
+  }
+});
 
 class App extends Component {
   render() {
     return (
-      <MuiThemeProvider>
+      <MuiThemeProvider muiTheme={main_theme}>
         <CodeEditor />
       </MuiThemeProvider>
     );
---
+++ src/colors.js
@@ -0,0 +1,14 @@
+var kiwiPurple = exports.kiwiPurple = '#6F4B70';
+var kiwiLightPurple = exports.kiwiLightPurple = "FDDCFF";
+var kiwiGreen = exports.kiwiGreen = '#27B881';
+var kiwiLightGreen = exports.kiwiLightGreen = '#B4EBC1';
+
+var kiwiDarkBlue = exports.kiwiDarkBlue = '#1253A4';
+var kiwiLightRed = exports.kiwiLightRed = '#f26964';
+var kiwiWhite = exports.kiwiWhite = '#f4f4f2';
+var kiwiYellow = exports.kiwiYellow = '#ffd464';
+
+var kiwiLimeGreen = exports.kiwiLimeGreen = '#A0FB71';
+var kiwiPastel = exports.kiwiPastel = '#58f8b';
+var kiwiLightBlue = exports.kiwiLightBlue = '#12f6e9';
+var kiwiDarkGreen = exports.kiwiDarkGreen = '#2f8f75';
