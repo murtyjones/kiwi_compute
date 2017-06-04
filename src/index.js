729598a88cb3e7aaddf70fc5183537fcf4a201fe
Scott Hudson <scott.w.hudson@gmail.com>
2017-06-03 19:44:15 -0500

    fix touchTap error

--- src/index.js
+++ src/index.js
@@ -4,9 +4,10 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
-
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
 injectTapEventPlugin();
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
+
