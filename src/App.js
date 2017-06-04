6146c9d4d11cf05209de4a72e219afebd27bdebe
patientplatypus <pweyand@gmail.com>
2017-06-03 20:28:13 -0500

    fuck life

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import CodeEditor from './Layouts/CodeEditor';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import axios from 'axios';
+import renderIf from 'render-if';
 
 import {kiwiGreen, kiwiLightGreen, kiwiPurple, kiwiLightPurple, kiwiBlue,
         kiwiDarkBlue, kiwiLightRed, kiwiWhite, kiwiYellow, kiwiLimeGreen,
@@ -34,11 +36,100 @@ const alt_theme2 = getMuiTheme({
   }
 });
 
+
+
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      username: '',
+      password: '',
+      loggedIn: false,
+      loginId: ''
+    }
+  }
+
+
+
+
+  loginHandle(e){
+    e.preventDefault();
+    console.log('inside loginHandle');
+    axios.post('http://localhost:5000/login',{
+      username: this.state.username,
+      password: this.state.password
+    })
+      .then((response)=>{
+          console.log('response from the login ' , response);
+          if (response.data.status==="passwordsmatch"){
+            this.setState({
+              loggedIn: true,
+              ulogged: "loggedin",
+              loginId: response.data.postid
+            })
+          }
+          if (response.data.status==="passwordsdontmatch"){
+            this.setState({
+              loggedIn: false,
+              ulogged: "error"
+            })
+          }
+        });
+  }
+
+  signupHandle(e){
+  e.preventDefault();
+  console.log('inside signupHandle');
+  axios.post('http://localhost:5000/signup',{
+    username: this.state.username,
+    password: this.state.password
+  })
+    .then((response)=>{
+        console.log('response from the signup ' , response);
+        if (response.data.status==="statusreject"){
+          this.setState({
+            loggedIn: false,
+            signedUp: "error"
+          })
+        }
+        if (response.data.status === "profileposted")
+          this.setState({
+            loggedIn: true,
+            signedUp: "signedup",
+            loginId: response.data.postid
+          })
+      });
+  }
+
   render() {
     return (
       <MuiThemeProvider muiTheme={main_theme}>
-        <CodeEditor />
+      <div>
+        <form>
+            <input
+                    onChange={(e)=>this.setState({username: e.target.value })}
+                    type="text"
+                    name="username"
+                    id="username"
+                    placeholder="username"/>
+            <input
+                    onChange={(e)=>this.setState({password: e.target.value })}
+                    type="text"
+                    name="password"
+                    id="password"
+                    placeholder="password"/>
+            <button onClick={(e)=>this.loginHandle(e)}>Login!</button>
+            <button onClick={(e)=>this.signupHandle(e)}>Sign Up!</button>
+        </form>
+
+        {renderIf(this.state.loggedIn === true)(
+          <CodeEditor loginId={this.state.loginId}/>
+        )}
+        {renderIf(this.state.loggedIn === false)(
+          <p>please log in to get to the code editor!</p>
+        )}
+
+      </div>
       </MuiThemeProvider>
     );
   }
--- src/Layouts/CodeEditor.js
+++ src/Layouts/CodeEditor.js
@@ -8,6 +8,7 @@ import React from 'react';
 import skulpt from 'skulpt';
 import Header from '../Components/Header';
 import SkulptIO from '../Components/SkulptIO';
+import axios from 'axios';
 
 class CodeEditor extends React.Component {
   constructor(props) {
@@ -16,6 +17,9 @@ class CodeEditor extends React.Component {
       editorInput: '',
       editorOutput: '',
       errorMsg: '',
+      userId:'',
+      text:'',
+      retrieved:true
     };
   }
 
@@ -54,6 +58,38 @@ class CodeEditor extends React.Component {
     }
   }
 
+      saveText(e){
+      e.preventDefault();
+      // console.log('inside savetext and the value of this.state')
+      axios.post('http://localhost:5000/savetext',{
+        userId:  this.props.loginId,
+        text: this.state.editorInput
+      })
+        .then((response)=>{
+            console.log('response from the signup ' , response);
+            this.setState({
+              text: ''
+            })
+          });
+    }
+
+    retrieveText(e){
+      var self = this;
+      e.preventDefault();
+      axios.post('http://localhost:5000/retrieveText',{
+        userId: this.props.loginId,
+      })
+        .then((response)=>{
+          console.log('response from retrieveText is ', response);
+            if (response.data.returnTexts.length>0){
+              self.setState({
+                returnTexts: response.data.returnTexts,
+                retrieved: true
+              })
+            }
+          });
+    }
+
   render() {
     return (
       <div>
@@ -66,6 +102,10 @@ class CodeEditor extends React.Component {
           inputUpdate={this.handleEditorChange}
           errorMsg={this.state.errorMsg}
         />
+        <form>
+          <button onClick={(e)=>this.saveText(e)}>Save Code!</button>
+          <button onClick={(e)=>this.retrieveText(e)}>Retrieve Code!</button>
+        </form>
       </div>
     );
   }
